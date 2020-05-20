213521fbbb905c6aec68407c74af2e9c5758d848
Thebarda <tomdar87@outlook.com>
2020-05-20 21:43:01 +0200

    reduce camera dead zone and and spot and torch light + player light occluder

--- scenes/Pivot.gd
+++ scenes/Pivot.gd
@@ -12,10 +12,4 @@ func _physics_process(delta):
 
 func update_pivot_angle():
 	var look_direction = player.look_direction
-	if player.velocity.y > 0:
-		look_direction.y = 1.6
-	elif player.velocity.y < 0:
-		look_direction.y = -0.6
-	else:
-		look_direction.y = -0.2
 	rotation = look_direction.angle()
